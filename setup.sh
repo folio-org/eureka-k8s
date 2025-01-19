997a5827ad6ad5663779d07970f8c67caedd5fbc
eldiiar-duishenaliev <Eldiiar_Duishenaliev@epam.com>
2025-01-19 14:14:30 +0600

    switch to bitnami repo for the kong

--- setup.sh
+++ setup.sh
@@ -62,22 +62,24 @@ kubectl create secret generic keycloak-credentials \
   --from-literal=KEYCLOAK_ADMIN_USER=$KEYCLOAK_ADMIN_USER \
   --namespace $1
 
-# Install Kong
-helm repo add kong https://charts.konghq.com
-helm repo update
-helm install kong-$1 kong/kong -f $current_dir/kong/values.yaml --version 12.0.11 --namespace $1
-
-# Install Keycloak
+# Add bitnami repo
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
+
+# Install Kong
+helm install kong-$1 bitnami/kong -f $current_dir/kong/values.yaml --version 12.0.11 --namespace $1
+
+# Install Keycloak
 helm install keycloak-$1 bitnami/keycloak -f $current_dir/keycloak/values.yaml --version 21.0.4 --namespace $1
 
 # Wait for Keycloak to be ready
-while [ $(kubectl get pods -l release=keycloak-$1 -o jsonpath='{.items[*].status.phase}' --namespace $1) != "Running" ]; do
+while [ $(kubectl get pods -l release=keycloak-$1 -o jsonpath='{.items[*].status.phase}' --namespace $1) != "Running" ]; 
+do
   sleep 5
 done
 
 # Wait for Kong to be ready
-while [ $(kubectl get pods -l release=kong-$1 -o jsonpath='{.items[*].status.phase}' --namespace $1) != "Running" ]; do
+while [ $(kubectl get pods -l release=kong-$1 -o jsonpath='{.items[*].status.phase}' --namespace $1) != "Running" ]; 
+do
   sleep 5
 done
